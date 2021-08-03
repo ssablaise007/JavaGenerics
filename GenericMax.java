731a01c4fe6fc3cd8ec549f84909215d53f5775a
Your Name <you@example.com>
2021-08-03 17:41:28 +0530

    Added : integer values Max

---
+++ GenericMax.java
@@ -0,0 +1,16 @@
+
+
+public class GenericMax {
+	public static <T extends Comparable<T>> T max(T... elements) {
+	    T max = elements[0];
+	    for (T element : elements) {
+	        if (element.compareTo(max) > 0) {
+	            max = element;
+	        }
+	    }
+	    return max;
+	}
+	public static void main(String[] args) {
+	       System.out.println("Integer Max: " + max(Integer.valueOf(32), Integer.valueOf(56), Integer.valueOf(89), Integer.valueOf(3), Integer.valueOf(456)));
+	       }
+}
